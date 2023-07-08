8d99a620b382701b79cb3f38ab74ce4cea3aa820
AnupPainuly <aanoop669@gmail.com>
2023-07-08 11:12:00 +0530

    --amend

--- gfg_solutions/missing_number_in_array.py
+++ gfg_solutions/missing_number_in_array.py
@@ -5,7 +5,6 @@ Intuition: sum of n natural numbers ```n*(n+1) // 2```
 '''
 class Solution:
     def missingNumber(self,array,n):
-        print("org arr sum", sum(array))
         total = n*(n+1) // 2
         return total -sum(array)
         
