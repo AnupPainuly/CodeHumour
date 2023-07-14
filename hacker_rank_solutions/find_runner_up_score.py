8bcf5a25780f489e01444291b936fc377847af49
AnupPainuly <aanoop669@gmail.com>
2023-07-14 22:26:02 +0530

    largest number in list

--- hacker_rank_solutions/find_runner_up_score.py
+++ hacker_rank_solutions/find_runner_up_score.py
@@ -1,13 +1,20 @@
+'''
+Objective: finding the second largest number in the list
+
+'''
+
+
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split(" ")[:n]))
-    print(arr)
-    # assume that the first element is maximun
-    max = arr[0]
-    for i in arr:
-        if i > max:
-            max = i
-    print(max)
-
-
+    # start with assumption that first ele is max and second ele is second_max
+    max_num = arr[0]
+    second_max = arr[1]
+    for i in range(2, len(arr)):
+        if arr[i] > max_num:
+            second_max = max_num
+            max_num = arr[i]
+        elif arr[i] > second_max and arr[i] != max_num:
+            second_max = arr[i]
+    print(second_max)
 
