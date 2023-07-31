8f3e5e6158ec06fae2090c6d40195b0dc9f952ba
AnupPainuly <aanoop669@gmail.com>
2023-07-31 19:10:52 +0530

    daily grind

---
+++ hacker_rank_solutions/intersection_set.py
@@ -0,0 +1,19 @@
+'''
+Sample Input
+
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+
+Sample Output
+
+5
+
+
+'''
+n = int(input())
+set_a = set(map(int, input().split(" ")[:n]))
+m = int(input())
+set_b = set(map(int, input().split(" ")[:m]))
+print(len(set_a.intersection(set_b)))
